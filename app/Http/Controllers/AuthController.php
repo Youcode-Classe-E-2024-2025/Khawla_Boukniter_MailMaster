b38645f8735d41c779039ea640a490832c2bd5ef
Khawla Boukniter <boukniter.khawla@gmail.com>
2025-04-09 10:45:58 +0100

    Install and configure Laravel Sanctum for authentication

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,134 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
+
+/**
+ * @OA\Info(
+ *    title="CarRental API",
+ *    version="1.0.0",
+ * )
+ * @OA\Tag(
+ *     name="Authentication",
+ *     description="API Endpoints for authentication"
+ * )
+ * @OA\SecurityScheme(
+ *     type="http",
+ *     scheme="bearer",
+ *     bearerFormat="JWT",
+ *     securityScheme="bearerAuth"
+ * )
+ */
+class AuthController extends Controller
+{
+
+    /**
+     * @OA\Post(
+     *     path="/api/register",
+     *     tags={"Authentication"},
+     *     summary="Register a new user",
+     *     @OA\RequestBody(
+     *         @OA\JsonContent(
+     *             required={"name","email","password"},
+     *             @OA\Property(property="name", type="string"),
+     *             @OA\Property(property="email", type="string"),
+     *             @OA\Property(property="password", type="string")
+     *         )
+     *     ),
+     *     @OA\Response(
+     *         response=201,
+     *         description="User registered",
+     *     )
+     * )
+     */
+    public function register(Request $request)
+    {
+        try {
+            $validated = $request->validate([
+                'name' => 'required',
+                'email' => 'required|unique:users|email',
+                'password' => 'required|min:6'
+            ]);
+
+            $user = User::create([
+                'name' => $validated['name'],
+                'email' => $validated['email'],
+                'password' => Hash::make($validated['password'])
+            ]);
+
+            $token = $user->createToken($request->name);
+
+            return [
+                'user' => $user,
+                'token' => $token->plainTextToken
+            ];
+        } catch (\Exception $e) {
+            return response()->json(['error' => $e->getMessage()], 500);
+        }
+    }
+
+    /**
+     * @OA\Post(
+     *     path="/api/login",
+     *     tags={"Authentication"},
+     *     summary="User login",
+     *     @OA\RequestBody(
+     *         @OA\JsonContent(
+     *             required={"email","password"},
+     *             @OA\Property(property="email", type="string"),
+     *             @OA\Property(property="password", type="string")
+     *         )
+     *     ),
+     *     @OA\Response(
+     *         response=200,
+     *         description="Login successful"
+     *     )
+     * )
+     */
+    public function login(Request $request)
+    {
+        try {
+            $validated = $request->validate([
+                'email' => 'required|exists:users|email',
+                'password' => 'required'
+            ]);
+
+            $user = User::where('email', $validated['email'])->first();
+
+            if (!$user || !Hash::check($request->password, $user->password)) {
+                return 'user not found or credentials are not correct';
+            }
+
+            $token = $user->createToken($user->name);
+
+            return [
+                'user' => $user,
+                'token' => $token->plainTextToken
+            ];
+        } catch (\Exception $e) {
+            return response()->json(['error' => $e->getMessage()], 500);
+        }
+    }
+
+    /**
+     * @OA\Post(
+     *     path="/api/logout",
+     *     tags={"Authentication"},
+     *     summary="User logout",
+     *     security={{ "bearerAuth": {} }},
+     *     @OA\Response(
+     *         response=200,
+     *         description="Logout successful"
+     *     )
+     * )
+     */
+    public function logout(Request $request)
+    {
+        $request->user()->tokens()->delete();
+
+        return 'logged out';
+    }
+}
