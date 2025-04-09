480b9b8b3d826f005561dca2fa278bf6b29727b9
Khawla Boukniter <boukniter.khawla@gmail.com>
2025-04-09 12:12:38 +0100

    generate swagger documentation and add routes for Newsletters, Subscribers, and Campaigns controllers

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,17 @@
 <?php
 
+use App\Http\Controllers\CampaignController;
+use App\Http\Controllers\NewsletterController;
+use App\Http\Controllers\SubscriberController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
+
+Route::apiResource('newsletters', NewsletterController::class);
+
+Route::apiResource('subscribers', SubscriberController::class);
+
+Route::apiResource('campaigns', CampaignController::class);
