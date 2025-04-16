b511fdd5ef66567ae447f6d11c82bcd7d1ffa8a8
Khawla Boukniter <boukniter.khawla@gmail.com>
2025-04-16 12:26:09 +0100

    Add tracking pixel to campaign email for open tracking

--- resources/views/emails/campaign.blade.php
+++ resources/views/emails/campaign.blade.php
@@ -3,6 +3,7 @@
 <body>
     <h1>{{ $campaign->subject }}</h1>
     <p>{{ $campaign->content }}</p>
+    <img src="{{ url('data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAQAAAC1HAwCAAAAC0lEQVR42mP8/wcAAwAB/LOZse8AAAAASUVORK5CYII=' . $campaign->id . '/email-open') }}" width="1" height="1" />
 </body>
 
 </html>
