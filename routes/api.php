91ea08bfc117aab143e542d3162cd8b01d542150
Khawla Boukniter <boukniter.khawla@gmail.com>
2025-04-16 11:50:30 +0100

    Add route for sending campaign emails in api.php

--- routes/api.php
+++ routes/api.php
@@ -14,6 +14,8 @@ Route::get('/user', function (Request $request) {
 Route::post('/register', [AuthController::class, 'register']);
 Route::post('/login', [AuthController::class, 'login']);
 
+Route::post('/campaigns/{id}/send', [CampaignController::class, 'sendCampaign']);
+
 Route::middleware(['auth:sanctum', 'web'])->group(function () {
     Route::post('/logout', [AuthController::class, 'logout']);
 
